cd354a40a5047cd76300c1a75dbbf5e4bdb5713a
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2025-01-15 21:20:32 +0200

    Did the MajorityElement.

---
+++ Easy/MajorityElement.java
@@ -0,0 +1,33 @@
+import java.util.Hashtable;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Hashtable<Integer, Integer> hmap = new Hashtable<Integer, Integer>();
+
+        int majorElementLimit = (nums.length)/2; 
+
+        for(int i = 0; i< nums.length; i++){
+
+            Integer c = hmap.get(nums[i]);
+
+            if (hmap.get(nums[i]) == null) {
+                hmap.put(nums[i], 1);
+                if(1>majorElementLimit){
+                    return nums[i];
+                }
+            }else {
+                
+                hmap.put(nums[i], ++c);
+                if(c>majorElementLimit){
+                    return nums[i];
+                }
+            }
+
+        }
+        return 0;
+    }
+
+    
+
+    
+}
