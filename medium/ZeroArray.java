614a41161cb2d2dc97cd20869361d86ae2b85d41
TheeSocerer <tshepo123@student.wethinkcode.co.za>
2025-02-02 13:00:42 +0200

    added the intuition to the zero array medium problem

--- medium/ZeroArray.java
+++ medium/ZeroArray.java
@@ -1,3 +1,14 @@
+/**
+ Key Idea
+Instead of applying every operation directly to nums (which would be inefficient), you:
+
+- Use a difference array deltaArray to efficiently mark operation ranges.
+
+- Compute a prefix sum on deltaArray to get the total number of operations at each position in the array.
+
+- Check if the number of operations is enough to bring each element of nums to 0.
+ */
+
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
 
